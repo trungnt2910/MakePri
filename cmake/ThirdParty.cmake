d306eeb9934fcc4ff8dd78bbc4d82cc3bc252522
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2026-08-04 21:20:52 +1000

    build: Migrate to CPM
    Otherwise, both `MakePri` and consumers will be forced to download
    their separate copies of Boost.

--- cmake/ThirdParty.cmake
+++ cmake/ThirdParty.cmake
@@ -1,95 +1,94 @@
-include(FetchContent)
+include("${CMAKE_CURRENT_LIST_DIR}/GetCPM.cmake")
 
 set(MAKEPRI_ORIGINAL_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
 list(JOIN MAKEPRI_THIRD_PARTY_COMPILE_FLAGS " " makepri_third_party_compile_flags)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${makepri_third_party_compile_flags}")
 set(MAKEPRI_ORIGINAL_BUILD_SHARED_LIBS "${BUILD_SHARED_LIBS}")
+set(BUILD_SHARED_LIBS FALSE)
 
 # ========================================================
 # libxml2
 # ========================================================
 if(NOT WIN32)
-    FetchContent_Declare(
-        libxml2
+    CPMAddPackage(
+        NAME libxml2
+        VERSION 2.15.3
         GIT_REPOSITORY https://gitlab.gnome.org/GNOME/libxml2.git
         GIT_TAG v2.15.3
-        GIT_SHALLOW TRUE
+        OPTIONS
+            "HAVE_DECL_GETENTROPY 0"
+            "LIBXML2_WITH_ICONV FALSE"
+            "LIBXML2_WITH_ICU FALSE"
+            "LIBXML2_WITH_LZMA FALSE"
+            "LIBXML2_WITH_MODULES FALSE"
+            "LIBXML2_WITH_PROGRAMS FALSE"
+            "LIBXML2_WITH_PYTHON FALSE"
+            "LIBXML2_WITH_TESTS FALSE"
+            "LIBXML2_WITH_THREADS FALSE"
+            "LIBXML2_WITH_ZLIB FALSE"
     )
-    set(BUILD_SHARED_LIBS OFF)
-    set(HAVE_DECL_GETENTROPY 0 CACHE INTERNAL "" FORCE)
-    set(LIBXML2_WITH_ICONV OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_ICU OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_LZMA OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_MODULES OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_PROGRAMS OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_PYTHON OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_TESTS OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_THREADS OFF CACHE BOOL "" FORCE)
-    set(LIBXML2_WITH_ZLIB OFF CACHE BOOL "" FORCE)
-    FetchContent_MakeAvailable(libxml2)
 endif()
-
-# ========================================================
-# Unicode Algorithms Implementation
-# ========================================================
-FetchContent_Declare(
-    uni-algo
-    URL https://github.com/uni-algo/uni-algo/archive/refs/tags/v1.2.0.tar.gz
-    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
-)
-set(UNI_ALGO_HEADER_ONLY ON CACHE BOOL "" FORCE)
-FetchContent_MakeAvailable(uni-algo)
 
 # ========================================================
 # Boost
 # ========================================================
 if(NOT WIN32)
-    set(BOOST_INCLUDE_LIBRARIES dll interprocess)
     string(
         CONCAT
         MAKEPRI_BOOST_URL
         "https://github.com/boostorg/boost/releases/download/boost-1.91.0-1/"
         "boost-1.91.0-1-cmake.tar.xz"
     )
-    FetchContent_Declare(
-        Boost
+    CPMAddPackage(
+        NAME Boost
+        VERSION 1.91.0
         URL "${MAKEPRI_BOOST_URL}"
         URL_HASH SHA256=cc5dc5006ecbdf0051f90979be31b4eee5987d9ae14ae9fb9c03cfa43fa3cdad
-        DOWNLOAD_EXTRACT_TIMESTAMP ON
-        EXCLUDE_FROM_ALL
+        DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+        EXCLUDE_FROM_ALL YES
+        OPTIONS "BOOST_INCLUDE_LIBRARIES dll\;interprocess"
     )
-    FetchContent_MakeAvailable(Boost)
     unset(MAKEPRI_BOOST_URL)
 endif()
+
+# ========================================================
+# Unicode Algorithms Implementation
+# ========================================================
+CPMAddPackage(
+    NAME uni-algo
+    VERSION 1.2.0
+    URL https://github.com/uni-algo/uni-algo/archive/refs/tags/v1.2.0.tar.gz
+    DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+    OPTIONS "UNI_ALGO_HEADER_ONLY TRUE"
+)
 
 # ========================================================
 # Windows Implementation Library
 # ========================================================
 if(WIN32)
-    FetchContent_Declare(
-        wil
+    CPMAddPackage(
+        NAME wil
+        VERSION 1.0.260126.7
         URL https://github.com/microsoft/wil/archive/refs/tags/v1.0.260126.7.tar.gz
         DOWNLOAD_EXTRACT_TIMESTAMP TRUE
+        OPTIONS "WIL_BUILD_TESTS FALSE"
     )
-    set(WIL_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-    FetchContent_MakeAvailable(wil)
     target_include_directories(WIL SYSTEM INTERFACE "$<BUILD_INTERFACE:${wil_SOURCE_DIR}/include>")
 endif()
 
 # ========================================================
 # Windows App SDK
 # ========================================================
-FetchContent_Declare(
-    windows_app_sdk #
-    # TODO: Switch to an official tag again once these PRs are merged:
-    # https://github.com/microsoft/WindowsAppSDK/pull/6663
-    # https://github.com/microsoft/WindowsAppSDK/pull/6664
+# TODO: Switch to an official tag again once these PRs are merged:
+# https://github.com/microsoft/WindowsAppSDK/pull/6663
+# https://github.com/microsoft/WindowsAppSDK/pull/6664
+CPMAddPackage(
+    NAME windows_app_sdk
     GIT_REPOSITORY https://github.com/trungnt2910/WindowsAppSDK.git
     GIT_TAG 55a9198c1d61cc6a0253131d791d3ecdec498164
     GIT_SHALLOW TRUE
     SOURCE_SUBDIR makepri-no-add-subdirectory
 )
-FetchContent_MakeAvailable(windows_app_sdk)
 include("${CMAKE_CURRENT_LIST_DIR}/MRTCore.cmake")
 mrtcore_add_targets("${windows_app_sdk_SOURCE_DIR}")
 
@@ -97,13 +96,12 @@ mrtcore_add_targets("${windows_app_sdk_SOURCE_DIR}")
 # GoogleTest
 # ========================================================
 if(BUILD_TESTING)
-    FetchContent_Declare(
-        googletest
+    CPMAddPackage(
+        NAME googletest
+        VERSION 1.14.0
         URL https://github.com/google/googletest/archive/refs/tags/v1.14.0.tar.gz
         DOWNLOAD_EXTRACT_TIMESTAMP TRUE
     )
-
-    FetchContent_MakeAvailable(googletest)
     target_compile_options(gtest PRIVATE -Wno-character-conversion)
 endif()
 
