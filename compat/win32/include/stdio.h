55497b1869ee715c483ee212d7c8909c85a86a76
Trung Nguyen <57174311+trungnt2910@users.noreply.github.com>
2026-08-06 20:51:47 +1000

    fix(compat): Fix UB with fclose(NULL)
    Win32 allows `fclose` to be called with `NULL`, while other
    implementations (e.g. Linux glibc) do not.
    
    We therefore wrap around the standard `stdio.h` header and redeclare the
    prototype for `fclose` to exclude the `nonnull` annotation.
    
    Without this wrapper, `fclose(file)` where `file` is `NULL` will trigger
    UBSan.

--- compat/win32/include/stdio.h
+++ compat/win32/include/stdio.h
@@ -1,6 +1,17 @@
 #pragma once
 
+#ifndef _WIN32
+// The prototpye for fclose on some systems has nonnull(1),
+// which triggers UBSan if fclose(NULL) is called.
+#define fclose system_fclose
+#endif
+
 #include_next <stdio.h>
+
+#ifndef _WIN32
+#undef fclose
+extern "C" int fclose(FILE* stream);
+#endif
 
 #include <corecrt_wstdio.h>
 
